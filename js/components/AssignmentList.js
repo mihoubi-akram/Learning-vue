9cf55fb94031a951b4b3b1254a4755c3cc39f5d2
Akram Fawzi Mihoubi <mihhakram@gmail.com>
2024-02-17 23:08:34 +0100

    add tag

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -6,9 +6,14 @@ export default {
         <h2 class="font-bold mb-2">
             {{title}} ({{assignments.length}})
         </h2>
+        <div class="flex gap-2">
+        <button @click="handleclick(tag)" v-for="tag in tags" class="border rounded px-1 py-px text-xs"
+        :class="{
+            'border-blue-500 text-blue-500': tag === currentTag }">{{ tag }}</button>
+        </div>
         <ul class ="border border-gray-600 divide-y divide-gray-600 mt-6">
             <assignment
-            v-for="assignment in assignments"
+            v-for="assignment in filteredAssignments"
             :key="assignment.id" 
             :assignment="assignment"> </assignment>
         </ul>
@@ -16,6 +21,31 @@ export default {
     props: {
         assignments: Array,
         title: String
+    },
+
+    data(){
+        return{
+           currentTag:"All", 
+        } 
+        
+    },
+    computed:{
+
+        filteredAssignments() {
+            if (this.currentTag === 'All') {
+                return this.assignments;
+            }
+
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        },
+        tags(){
+            return ['All', ...new Set(this.assignments.map(a => a.tag))];
+        }
+    },
+    methods:{
+        handleclick(tag){
+            this.currentTag = tag
+        }
     }
 
 }
